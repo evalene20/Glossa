413717c4038b04254984bebbb92dc80449cf8473
evalene20 <eshaevalene20@gmail.com>
2026-06-27 00:49:21 +0400

    added more languages

---
+++ app/languages.py
@@ -0,0 +1,25 @@
+LANGUAGES = {
+    "English": "eng_Latn",
+    "Hindi": "hin_Deva",
+    "French": "fra_Latn",
+    "German": "deu_Latn",
+    "Spanish": "spa_Latn",
+    "Arabic": "arb_Arab",
+    "Japanese": "jpn_Jpan",
+    "Chinese (Simplified)": "zho_Hans",
+    "Russian": "rus_Cyrl",
+    "Korean": "kor_Hang"
+}
+
+LANGUAGE_DETECT_MAP = {
+    "en": "eng_Latn",
+    "hi": "hin_Deva",
+    "fr": "fra_Latn",
+    "de": "deu_Latn",
+    "es": "spa_Latn",
+    "ar": "arb_Arab",
+    "ja": "jpn_Jpan",
+    "zh-cn": "zho_Hans",
+    "ru": "rus_Cyrl",
+    "ko": "kor_Hang"
+}
